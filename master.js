37e89e4dc7286384b356367c5948cd89135e5959
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-04-12 19:39:48 -0400

    preparing to deliver to the teacher with css format

--- master.js
+++ master.js
@@ -1,4 +1,4 @@
-let who = ['the dog','my granma','his turtle','my bird'];
+let who = ['The dog','My granma','His turtle','My bird'];
 let what = ['eat','pissed','crushed','broked'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
